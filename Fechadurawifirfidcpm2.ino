a300f3694c414e254465b0293bc5d099d0bcd6c9
Alan Pizzo <114959678+alanpizzo@users.noreply.github.com>
2024-03-25 20:36:37 -0300

    Update Fechadurawifirfidcpm2.ino
    Código alterado para que o projeto seja controlado por uma placa esp32.

--- Fechadurawifirfidcpm2.ino
+++ Fechadurawifirfidcpm2.ino
@@ -1,48 +1,48 @@
 /*==================================================================================================
-|
-|                           2   C  P  M
-|
-|             F E C H A D U R A   E L E T R Ô N I C A     R F I D
-====================================================================================================
+  |
+  |                           2   C  P  M
+  |
+  |             F E C H A D U R A   E L E T R Ô N I C A     R F I D
+  ====================================================================================================
 
-Bibliotecas
-====================================================================================================*/
+  Bibliotecas
+  ====================================================================================================*/
 #include <SPI.h>      //biblioteca protocolo SPI
 #include <MFRC522.h>  //biblioteca leitor RFID
 
 /* Variáveis e definições
-====================================================================================================*/
-#define PINO_SDA 10        //pino SDA do leitor RFID
-#define PINO_RST 9         //pino RST do leitor RFID
-#define pinoLedvermelho 2  //pino do led vermelho
-#define pinoLedverde 3     //pino do led verde
-#define pinoTrava 4        //pino do solenóide
+  ====================================================================================================*/
+#define PINO_SDA 21         //pino SDA do leitor RFID
+#define PINO_RST 22         //pino RST do leitor RFID
+#define pinoLedvermelho 32  //pino do led vermelho
+#define pinoLedverde 12     //pino do led verde
+#define pinoTrava 33        //pino do solenóide
 
 
 /*Instanciação de objetos
-===================================================================================================*/
+  ===================================================================================================*/
 MFRC522 mfrc522(PINO_SDA, PINO_RST);  //Cria objeto para leitor RFID
 
 /*Protótipo da função
-====================================================================================================*/
+  ====================================================================================================*/
 void rfid_func();  //Identificação de tags RFID
 
 
 void setup() {
   /*Modo de operação dos pinos
-====================================================================================================*/
+    ====================================================================================================*/
   pinMode(pinoLedverde, OUTPUT);     //pino do led verde
   pinMode(pinoLedvermelho, OUTPUT);  //pino do led vermelho
   pinMode(pinoTrava, OUTPUT);        //saída para trava eletrônica
 
   /*Inicialização dos leds e da trava
-====================================================================================================*/
-  digitalWrite(pinoLedvermelho, LOW);  //desliga pinoBuzzer
-  digitalWrite(pinoLedverde, LOW);     //desliga pinoBuzzer
+    ====================================================================================================*/
+  digitalWrite(pinoLedvermelho, LOW);  //desliga led vermelho
+  digitalWrite(pinoLedverde, LOW);     //desliga led verde
   digitalWrite(pinoTrava, LOW);        //desliga trava
 
   /*Inicialização da comunicação serial e leitor RFID
-====================================================================================================*/
+    ====================================================================================================*/
   Serial.begin(9600);  //inicializa Serial
   SPI.begin();         //inicializa SPI
   mfrc522.PCD_Init();  //inicializa o leitor RFID
@@ -60,7 +60,7 @@ void loop() {
 }
 
 /*Funções
-====================================================================================================*/
+  ====================================================================================================*/
 void rfid_func()  //Função para identificação de tags RFID
 {
   // -- Verifica novas tags --
@@ -86,7 +86,7 @@ void rfid_func()  //Função para identificação de tags RFID
 
 
   //tags autorizadas a acessar a fechadura
-  if (conteudo.substring(1) == "E7 31 77 53" || conteudo.substring(1) == "60 2F b8 89") {
+  if (conteudo.substring(1) == "99 05 80 72" || conteudo.substring(1) == "E7 31 77 53" || conteudo.substring(1) == "60 2F B8 89") {
     //Mensagem para acesso negado
     Serial.println("Autorizado!");
     Serial.println();
